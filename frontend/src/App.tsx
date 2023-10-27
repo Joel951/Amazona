58f412639753eda91877227c92438e2601e51821
Joel951 <joeljames951@gmail.com>
2023-10-27 12:36:33 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { sampleProducts } from "./data";
+
+function App() {
+  return (
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Amazona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/sigin" className="nav-link">
+              SignIn
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.image}
+                  className="product_image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
